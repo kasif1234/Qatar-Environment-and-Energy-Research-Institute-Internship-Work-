1e6874e24bf47d3e688030de8dbd6cf6ff3db384
kasif1234 <rebornland619@gmail.com>
2026-04-12 12:40:08 +0300

    implemented scalable python code for statistical analysis on OTE figures -> main extraction csv

--- Scalable_Work/Scalable_MATLAB_CODE.m
+++ Scalable_Work/Scalable_MATLAB_CODE.m
@@ -1,2 +1,11 @@
 %Establish all 5 graphs, transcribe video data into excel, then import into MATLAB.
 %Use the following code to plot the data from excel, and then use the"plot" function to create the graphs.
+
+% Taylor Sparks - TE Polymers
+% (1) Bar Chart - Distribution of sysTEm dataset according to the publishing date of each point.
+% (2) Box Plots
+% (3) Histogram Plots  
+% (4) Pearson Correlation
+%
+
+
