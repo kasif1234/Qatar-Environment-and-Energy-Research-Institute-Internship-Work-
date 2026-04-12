cdc1302c53c0c031d8339296efcc8595c16f124d
kasif1234 <rebornland619@gmail.com>
2026-04-12 08:41:07 +0300

    matlab intstuctions added, we have to scale more, perhaps try simulink

---
+++ Scalable_Work/Scalable_MATLAB_CODE.m
@@ -0,0 +1,2 @@
+%Establish all 5 graphs, transcribe video data into excel, then import into MATLAB.
+%Use the following code to plot the data from excel, and then use the"plot" function to create the graphs.
