69e40a1eac3a498ecc11fcaebe31a07dbb22af1d
kasif1234 <rebornland619@gmail.com>
2026-04-13 21:17:36 +0300

    update

--- Scalable_Work/Scalable_Python_Code.py
+++ Scalable_Work/Scalable_Python_Code.py
@@ -1,8 +1,21 @@
+#=================================================================================================
+# Libraries:
 # 1. pandas — to load, clean, and organize the CSV data into tables you can analyze easily.
 # 2. matplotlib — to create the bar chart, box plots, and histogram plots.
 # 3. scipy — to calculate the Pearson correlation and other statistical values.
 # 4. numpy — to handle numerical operations efficiently behind the scenes.
 # 5. seaborn — to make the plots look cleaner and more polished with less code.
+#=================================================================================================
+# Types of Plots:
+# 1. Bar Chart: To compare the average values of a specific column across different categories.
+# 2. Box Plot: To visualize the distribution of a numerical column and identify outliers
+# 3. Histogram: To show the frequency distribution of a numerical column and understand its shape.
+# 4. Pearson Correlation: To measure the strength and direction of the linear relationship between two numerical columns.
+#=================================================================================================
+# Graphs we can make now - Transcribed from 08/04/2026 Meeting:
+
+# https://chatgpt.com/c/69dd0506-45f4-8392-9f9b-efca2313721a
+
 
 import pandas as pd
 
@@ -25,4 +38,17 @@ df = pd.read_excel(file_path, sheet_name=selected_sheet, engine="openpyxl", head
 
 print(f"\nLoaded sheet: {selected_sheet}")
 print(df.head())
-print(f"\nColumns in the sheet: {df.columns.tolist()}")
+print(f"\nColumns in the sheet: {df.columns.tolist()[0:10]}")  # Show first 10 columns for brevity
+
+# Make dictionary: key -> column name
+column_dict = {i: col for i, col in enumerate(df.columns, start=1)}
+
+print("\nColumn dictionary:")
+for k, v in column_dict.items():
+    print(f"{k}: {v}")
+
+
+for k, v in column_dict.items():
+    if k == 3:
+        for index, value in df[v].items():
+            print(f"Row {index}: {value}")
